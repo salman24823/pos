54658ff92f09a1e71186183b6f0d316f19383381
abdulhamad222 <abdulhamad373@gmail.com>
2025-06-17 17:20:40 +0500

    registration_update

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,8 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 import { HeroUIProvider } from "@heroui/react";
+import { ToastContainer } from "react-toastify";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,6 +23,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="dark">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <ToastContainer />
         <HeroUIProvider>
           {children}
         </HeroUIProvider>
